63c92ba4e68c4de05c4a67c18a104c0d0b5e55de
Tung Duong Bui <tdbui1209@gmail.com>
2021-07-18 05:06:14 +0700

    add exercise 8.1

---
+++ Chapter 8/ex8.1.py
@@ -0,0 +1,20 @@
+'Python'[1] # 'y'
+'Strings are sequences of characters.'[5] # 'g'
+len('wonderful') # 9
+'Mystery'[:4] # 'Myst'
+'p' in 'Pineapple' # True
+'apple' in 'Pineapple' # True
+'pear' not in 'Pineapple' # True
+'apple' > 'pineapple' # False
+'pineapple' < 'Peach' # False
+
+# Test
+print('Python'[1] == 'y')
+print('Strings are sequences of characters.'[5] == 'g')
+print(len('wonderful') == 9)
+print('Mystery'[:4] == 'Myst')
+print(('p' in 'Pineapple') == True)
+print(('apple' in 'Pineapple') == True)
+print(('pear' not in 'Pineapple') == True)
+print(('apple' > 'pineapple') == False)
+print(('pineapple' < 'Peach') == False)
