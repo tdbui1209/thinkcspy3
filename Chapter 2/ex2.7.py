384ed4e358b08b063a8fcd9622f836a27ae217a9
Tung Duong Bui <tdbui1209@gmail.com>
2021-07-17 23:23:08 +0700

    change hours_per_day at go_off to hours_of_day

--- Chapter 2/ex2.7.py
+++ Chapter 2/ex2.7.py
@@ -1,5 +1,5 @@
 hours_of_day = 24
 current_time = 14 # 2pm
 set_alarm = 5100
-go_off = (current_time + set_alarm) % hours_per_day
+go_off = (current_time + set_alarm) % hours_of_day
 print('The alarm goes off at', go_off, ': 00')
