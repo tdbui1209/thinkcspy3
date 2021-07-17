f9c05b5e25bdc3284a9aa812549871eb9aa3d762
Tung Duong Bui <tdbui1209@gmail.com>
2021-07-17 19:19:22 +0700

    add Chapter 2

---
+++ Chapter 2/ex2.1.py
@@ -0,0 +1,14 @@
+# Store each word in a separate variable.
+a = 'All'
+b = 'work'
+c = 'and'
+d = 'no'
+e = 'play'
+f = 'makes'
+g = 'Jack'
+h = 'a'
+i = 'dull'
+j = 'boy'
+
+# Print out the sentence on one line
+print(a, b, c, d, e, f, g, h, i, j)
