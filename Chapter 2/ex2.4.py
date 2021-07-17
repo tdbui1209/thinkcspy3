b84abd0bd571d40a764fc7209dd5f8866e1c253a
Tung Duong Bui <tdbui1209@gmail.com>
2021-07-17 19:34:31 +0700

    add ex2.4.py

---
+++ Chapter 2/ex2.4.py
@@ -0,0 +1,8 @@
+# the following line will raise an error
+# print(bruce + 4)
+# because bruce is is not defined
+
+# Assign a value to bruce so that bruce + 4 evaluates to 10
+
+bruce = 6
+print(bruce + 4) # 10
