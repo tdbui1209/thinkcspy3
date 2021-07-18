428532a4c031530a2e38a24aa64b344827d02940
Tung Duong Bui <tdbui1209@gmail.com>
2021-07-18 21:22:50 +0700

    exercise 8.11

---
+++ Chapter 8/ex8.11.py
@@ -0,0 +1,20 @@
+from unit_test import test
+
+def count(substring, s):
+    idx = 0
+    count = 0
+    while idx < len(s):
+        if s[idx::len(substring)] == substring:
+            count += 1
+        idx += 1
+    return count
+
+def test_suite():
+    test(count("is", "Mississippi") == 2)
+    test(count("an", "banana") == 2)
+    test(count("ana", "banana") == 2)
+    test(count("nana", "banana") == 1)
+    test(count("nanan", "banana") == 0)
+    test(count("aaa", "aaaaaa") == 4)
+
+test_suite()
